c484c5867e2a007e31abddf1c3fea4c10c624571
Collatty <hakoncb@gmail.com>
2019-04-02 12:16:39 +0200

    Random commit

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,31 @@
+import Components.State;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Client {
 
     public Client(BufferedReader serverMessages) {
+        System.out.println();
 
     }
 
     public static void main(String[] args) {
 
+        System.out.println("hello world");
         BufferedReader serverMessages = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            State initialState = new State(serverMessages);
+            System.out.println(initialState.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
 
         Client client = new Client(serverMessages);
+
         //TODO main method for running client program
     }
 }
--- src/main/java/Components/Agent.java
+++ src/main/java/Components/Agent.java
@@ -1,6 +1,21 @@
 package Components;
 
-public class Agent {
+import java.util.Observable;
+import java.util.Observer;
+
+public class Agent implements Observer {
+
+    public final String name;
+
+    public Agent (String name){
+        this.name = name;
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 
 
     //TODO implement agents
