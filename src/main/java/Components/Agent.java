222f2036636a783a3603791eadcabae7fa5aa046
Collatty <hakoncb@gmail.com>
2019-04-05 15:12:46 +0200

    State representation

--- src/main/java/Components/Agent.java
+++ src/main/java/Components/Agent.java
@@ -5,12 +5,52 @@ import java.util.Observer;
 
 public class Agent implements Observer {
 
-    public final String name;
 
-    public Agent (String name){
+
+    private final char name;
+    private final String color;
+    private int yCoord;
+    private int xCoord;
+
+
+    public Agent (char name, String color, int xCoord, int yCoord){
         this.name = name;
+        this.color = color;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+
 
     }
+
+
+    //GETTERS
+    public char getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getxCoord() {
+        return xCoord;
+    }
+
+    public int getyCoord() {
+        return yCoord;
+    }
+    //END GETTERS
+
+
+    //SETTERS
+    public void setxCoord(int xCoord) {
+        this.xCoord = xCoord;
+    }
+
+    public void setyCoord(int yCoord) {
+        this.yCoord = yCoord;
+    }
+    //END SETTERS
 
     @Override
     public void update(Observable o, Object arg) {
