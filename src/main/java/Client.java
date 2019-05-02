97820df64251f44ac65e387cd41acf5ce31b8fb3
JonasBK <jonas.bulow@hotmail.com>
2019-05-02 22:54:47 +0200

    Format Task.java and Client.java to standard java formatting and organize imports

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,95 +1,89 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import AI.Heuristic;
-import Components.*;
-import Components.State.State;
-import Utilities.LevelReader;
+import Components.Agent;
+import Components.BlackBoard;
+import Components.Color;
+import Components.Task;
 
 public class Client {
 
     public Client(BufferedReader serverMessages) {
-        System.out.println();
+	System.out.println();
 
     }
 
-    public static void main(String[] args) throws IOException{
-        /*
-        BufferedReader serverMessages = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Ballefrans"); //CLIENTNAME - INITATING SERVER COMMUNICATION
+    public static void main(String[] args) throws IOException {
+	/*
+	 * BufferedReader serverMessages = new BufferedReader(new
+	 * InputStreamReader(System.in)); System.out.println("Ballefrans"); //CLIENTNAME
+	 * - INITATING SERVER COMMUNICATION
+	 * 
+	 * 
+	 * //READING IN LEVEL INFORMATION FROM SERVER
+	 * LevelReader.stringCreator(LevelReader.readAllLines(serverMessages)); try {
+	 * //oldState initialOldState = new oldState(LevelReader.getInitial(),
+	 * LevelReader.getGoals());
+	 * 
+	 * State state = new State();
+	 * 
+	 * Heuristic heuristic = new Heuristic() {
+	 * 
+	 * @Override public int compare(State o1, State o2) { return 0; } };
+	 * 
+	 * heuristic.h(state);
+	 * 
+	 * System.err.println("Heuristic: " + heuristic.toString());
+	 * 
+	 * 
+	 * } catch (Exception e) { e.printStackTrace(); }
+	 * //System.out.println("Move(W);Move(E)"); String response =
+	 * serverMessages.readLine(); System.err.println("Serverresponse is: " +
+	 * response);
+	 * 
+	 * 
+	 */
+	ArrayList<Agent> agents = new ArrayList<>();
+	ArrayList<Task> tasks = new ArrayList<>();
 
+	// HARDCODED TEST
+	Task t1 = new Task(4, 1, new ArrayList<>());
+	Task t2 = new Task(1, 3, new ArrayList<>());
+	t1.setColor(Color.RED);
+	t2.setColor(Color.BLUE);
+	ArrayList<Long> dep = new ArrayList<Long>();
+	dep.add(t1.getId());
+	t2.setDependencies(dep);
+	tasks.add(t1);
+	tasks.add(t2);
 
-        //READING IN LEVEL INFORMATION FROM SERVER
-        LevelReader.stringCreator(LevelReader.readAllLines(serverMessages));
-        try {
-            //oldState initialOldState = new oldState(LevelReader.getInitial(), LevelReader.getGoals());
+	// BlackBoard blackboard = new BlackBoard(SubGoalPlanner.convertToTask());
 
-            State state = new State();
+	// TODO: should be the real initial state
+	// State initState = new State();
+	BlackBoard blackboard = new BlackBoard(tasks);
 
-            Heuristic heuristic = new Heuristic() {
-                @Override
-                public int compare(State o1, State o2) {
-                    return 0;
-                }
-            };
+	// HARDCODED TEST
+	Agent a0 = new Agent(0, Color.RED, 1, 1, blackboard);
+	Agent a1 = new Agent(1, Color.BLUE, 4, 3, blackboard);
+	// Agent a2 = new Agent(2, Color.RED, 2, 3, blackboard);
+	// Agent a3 = new Agent(3, Color.RED, 3, 2, blackboard);
+	// Agent a4 = new Agent(4, Color.RED, 7, 1, blackboard);
+	// Agent a5 = new Agent(5, Color.RED, 6, 3, blackboard);
 
-            heuristic.h(state);
+	agents.add(a0);
+	agents.add(a1);
+	// agents.add(a2);
+	// agents.add(a3);
+	// agents.add(a4);
+	// agents.add(a5);
 
-            System.err.println("Heuristic: " + heuristic.toString());
+	blackboard.setAgents(agents);
+	blackboard.start();
 
+	// Client client = new Client(serverMessages);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //System.out.println("Move(W);Move(E)");
-        String response = serverMessages.readLine();
-        System.err.println("Serverresponse is: " + response);
-
-
-        */
-        ArrayList<Agent> agents = new ArrayList<>();
-        ArrayList<Task> tasks = new ArrayList<>();
-
-        // HARDCODED TEST
-        Task t1 = new Task(4,1, new ArrayList<>());
-        Task t2 = new Task(1,3, new ArrayList<>());
-        t1.setColor(Color.RED);
-        t2.setColor(Color.BLUE);
-        ArrayList<Long> dep = new ArrayList<Long>();
-        dep.add(t1.getId());
-        t2.setDependencies(dep);
-        tasks.add(t1);
-        tasks.add(t2);
-
-        //BlackBoard blackboard = new BlackBoard(SubGoalPlanner.convertToTask());
-
-        //TODO: should be the real initial state
-        //State initState = new State();
-        BlackBoard blackboard = new BlackBoard(tasks);
-
-        // HARDCODED TEST
-        Agent a0 = new Agent(0, Color.RED, 1, 1, blackboard);
-        Agent a1 = new Agent(1, Color.BLUE, 4, 3, blackboard);
-        //Agent a2 = new Agent(2, Color.RED, 2, 3, blackboard);
-        //Agent a3 = new Agent(3, Color.RED, 3, 2, blackboard);
-        //Agent a4 = new Agent(4, Color.RED, 7, 1, blackboard);
-        //Agent a5 = new Agent(5, Color.RED, 6, 3, blackboard);
-
-        agents.add(a0);
-        agents.add(a1);
-        //agents.add(a2);
-        //agents.add(a3);
-        //agents.add(a4);
-        //agents.add(a5);
-
-        blackboard.setAgents(agents);
-        blackboard.start();
-
-
-        //Client client = new Client(serverMessages);
-
-        //TODO main method for running client program
+	// TODO main method for running client program
     }
 }
--- src/main/java/Components/Task.java
+++ src/main/java/Components/Task.java
@@ -1,6 +1,5 @@
 package Components;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //TODO: Consider if attributes should be private and have getters and setters
@@ -14,63 +13,61 @@ public class Task {
     private TaskType taskType;
 
     public long getId() {
-        return id;
+	return id;
     }
 
     public void setId(long id) {
-        this.id = id;
+	this.id = id;
     }
 
     public int getRow() {
-        return row;
+	return row;
     }
 
     public void setRow(int row) {
-        this.row = row;
+	this.row = row;
     }
 
     public int getCol() {
-        return col;
+	return col;
     }
 
     public void setCol(int col) {
-        this.col = col;
+	this.col = col;
     }
 
     public Color getColor() {
-        return color;
+	return color;
     }
 
     public void setColor(Color color) {
-        this.color = color;
+	this.color = color;
     }
 
     public List<Long> getDependencies() {
-        return dependencies;
+	return dependencies;
     }
 
     public void setDependencies(List<Long> dependencies) {
-        this.dependencies = dependencies;
+	this.dependencies = dependencies;
     }
 
     public TaskType getTaskType() {
-        return taskType;
+	return taskType;
     }
 
     public void setTaskType(TaskType taskType) {
-        this.taskType = taskType;
+	this.taskType = taskType;
     }
 
-
     public Task(int row, int col, List<Long> dependencies) {
-        this.row = row;
-        this.col = col;
-        this.dependencies = dependencies;
+	this.row = row;
+	this.col = col;
+	this.dependencies = dependencies;
     }
 
     public Task() {
 
     }
-
 
 }
